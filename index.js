7df673881fa13a96e14274f8484eb7ed189f9320
pablo128482 <ashley.c.clay@gmail.com>
2023-05-19 15:25:42 -0700

    commit

---
+++ index.js
@@ -0,0 +1,55 @@
+function add(){
+1 + 80; //=> 81
+}
+
+function subtract(){
+    60 - 40; //=> 20    
+}
+
+function multiply(){
+2 * 3.4; //=> 6.8   
+}
+
+function divide (){
+    5.0 / 2.5; //=> 2    
+}
+
+function add(a, b){
+return a + b;
+}
+
+function subtract(a, b){
+return a - b;
+}
+
+function multiply(a, b){
+return a * b;    
+}
+
+function divide(a, b){
+return a / b;    
+}
+
+function increment(n){
+     n++; //=> 1
+     return n
+}
+
+function decrement(n){
+    n--; //=> 1
+    return n
+}
+
+function makeInt(n){
+   //console.log(n)
+   //return (n)
+return parseInt(n, 10); //=> 2
+    //return (isNaN(parsed))
+}
+
+function preserveDecimal(n){
+   //console.log(n)
+   //return (n)
+return parseFloat(n, 10); //=> 2
+    //return (isNaN(parsed))
+}
